267edc5006fc4e634d63a0464d782de7efc5c483
ndemia <info@demia.me>
2021-11-17 22:41:46 +0100

    Added some variables and stuff

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log('read')
+document.querySelector('.form').addEventListener('submit', function(e) {
+
+    e.preventDefault();
+
+    let departureLocation = document.querySelector('#departure').value;
+    let arrivalLocation = document.querySelector('#arrival').value;
+    let departureDate = document.querySelector('#departure-date').value;
+    let returnDate = document.querySelector('#arrival-date').value;
+    
+
+});
