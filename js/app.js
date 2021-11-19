b57f0496b1ecd55db5d8cf90b1dff172a806a4fc
ndemia <info@demia.me>
2021-11-19 17:18:33 +0100

    Create localStorage instructions

--- js/app.js
+++ js/app.js
@@ -5,7 +5,23 @@ document.querySelector('.form').addEventListener('submit', function(e) {
     let departureLocation = document.querySelector('#departure').value;
     let arrivalLocation = document.querySelector('#arrival').value;
     let departureDate = document.querySelector('#departure-date').value;
-    let returnDate = document.querySelector('#arrival-date').value;
+    let returnDate = document.querySelector('#return-date').value;
+
+    // Create flight information object
+    const flight = [
+        { 
+            departureLocation: departureLocation,
+            arrivalLocation: arrivalLocation,
+            departureDate: departureDate,
+            returnDate: returnDate
+        }
+    ]
+
+    // Store data
+    localStorage.setItem('flightInformation', JSON.stringify(flight));
     
+    // Get data
+    const testData = JSON.parse(localStorage.getItem('flightInformation'));
+
 
 });
