41c3d95cb52d19841f5550eafdb56080a253a656
ndemia <info@demia.me>
2022-06-02 18:20:39 +0200

    Added missing code regarding writing to localStorage

--- js/app.js
+++ js/app.js
@@ -10,12 +10,13 @@ const checkDarkMode = function () {
 	}
 };
 
-const checkForPreviousFlights = function() {
+
+const checkForPreviousFlights = function () {
 
     if (localStorage.getItem('flightInformation')) {
         
         // Save previous flight information
-        const previousFlightSearches = JSON.parse(localStorage.getItem('flightInformation'));
+        let previousFlightSearches = JSON.parse(localStorage.getItem('flightInformation'));
         
         // Show it on the UI
         document.querySelector('.search__departure').innerText = `${previousFlightSearches[0].departureLocation}`;
@@ -27,38 +28,59 @@ const checkForPreviousFlights = function() {
 
 };
 
-checkForPreviousFlights();
-
+// Overall form behaviour
 document.querySelector('.form').addEventListener('submit', function(e) {
 
     e.preventDefault();
-
+    
     let departureLocation = document.querySelector('#departure').value;
     let arrivalLocation = document.querySelector('#arrival').value;
     let departureDate = document.querySelector('#departure-date').value;
     let returnDate = document.querySelector('#return-date').value;
 
-    if(localStorage.getItem('flightInformation')) {
-        
-    }
-
-    // Create flight information object
-    const flight = [
+    // Create flight information object that will store the flight search information
+    let flight = [
         { 
             departureLocation: departureLocation,
             arrivalLocation: arrivalLocation,
             departureDate: departureDate,
             returnDate: returnDate
         }
-    ]
+    ];
 
-    // Store data
-    localStorage.setItem('flightInformation', JSON.stringify(flight));
+    // Check if previous flight searches have been made, so that we will add to them instead of overwrite them
+    if (localStorage.getItem('flightInformation')) {
+        
+        // Save those previous searches as array
+        let previousFlight = JSON.parse(localStorage.getItem('flightInformation'));
+        
+        // We add the new search to it. I reference the index so that way only the object inside the array is added, and not the whole array. This way I avoid
+        // adding unnecessary brackets to the string that will later be stored
+        previousFlight.push(flight[0]);
+        
+        localStorage.setItem('flightInformation', JSON.stringify(previousFlight));
 
-    // Reload page
+    } else {
+
+        // If no previous searches have been stored, just proceed with saving the current one
+        localStorage.setItem('flightInformation', JSON.stringify(flight));
+
+    }
+
+    // Reload page after submit
     window.location.reload();
 
+});
+
+// Delete stored searches on request
+document.querySelector('.btn--clear').addEventListener('click', function() {
+
+    localStorage.clear();
+
+    // Reload page after deleting
+    window.location.reload();
 
 });
 
 document.addEventListener('DOMContentLoaded', checkDarkMode());
+document.addEventListener('DOMContentLoaded', checkForPreviousFlights());
